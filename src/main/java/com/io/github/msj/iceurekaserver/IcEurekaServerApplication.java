5ac13cdd16fa48467d65496a4298bf8799f631ad
Mateus-msj3 <mateussouzaads@outlook.com>
2022-10-06 21:30:39 -0300

    first commit

---
+++ src/main/java/com/io/github/msj/iceurekaserver/IcEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.msj.iceurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcEurekaServerApplication.class, args);
+	}
+
+}
